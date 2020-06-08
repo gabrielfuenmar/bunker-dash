eaf4694c8a6ca71be83582f3727b4fe342b264e8
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2020-06-08 06:39:41 +0200

    Update app.py
    Add server

--- app.py
+++ app.py
@@ -485,6 +485,7 @@ app = dash.Dash(__name__,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 app.title="Bunker Analytics"
+server=app.server
 
 #Authentication
 auth = dash_auth.BasicAuth(app,VALID_USERNAME_PASSWORD_PAIRS)
